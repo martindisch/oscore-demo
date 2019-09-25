4da52cf325eb3b0f54c42ded8ec739a565937dcb
Martin Disch <martindisch@gmail.com>
2019-09-25 11:41:05 +0200

    Panic and print over ITM instead of semihosting

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate panic_itm;
 
-use cortex_m::asm;
+use cortex_m::{iprintln, Peripherals};
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let mut p = Peripherals::take().unwrap();
+    let stim = &mut p.ITM.stim[0];
 
-    loop {
-        // your code goes here
-    }
+    iprintln!(stim, "Hello, world!");
+    panic!("Oh, snap!");
+
+    loop {}
 }
