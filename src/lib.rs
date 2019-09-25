1f555e538a8b7c632ab82d58cfb36cfe5a11e83d
Martin Disch <martindisch@gmail.com>
2019-09-25 15:26:55 +0200

    Move initialization into function

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,30 @@
 #![no_std]
 
+use cortex_m::peripheral::ITM;
 use f3::{
-    hal::{delay::Delay, prelude::*},
+    hal::{delay::Delay, prelude::*, stm32f30x},
     led::Leds,
 };
 
 mod macros;
+
+/// Returns the `ITM`, `Leds` and `Delay`.
+pub fn init() -> (ITM, Leds, Delay) {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f30x::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let gpioe = dp.GPIOE.split(&mut rcc.ahb);
+
+    // clock configuration using the default settings (all clocks run at 8 MHz)
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let leds = Leds::new(gpioe);
+    let delay = Delay::new(cp.SYST, clocks);
+
+    (cp.ITM, leds, delay)
+}
 
 /// Lights the LEDs in the circle one by one.
 pub fn spin_leds(delay: &mut Delay, leds: &mut Leds) {
--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,15 @@ extern crate panic_itm;
 
 use cortex_m::iprintln;
 use cortex_m_rt::entry;
-use f3::{
-    hal::{delay::Delay, prelude::*, stm32f30x},
-    led::Leds,
-};
 
 use oscore_demo::siprintln;
 
 #[entry]
 fn main() -> ! {
-    let mut cp = cortex_m::Peripherals::take().unwrap();
-    let dp = stm32f30x::Peripherals::take().unwrap();
-
-    let stim = &mut cp.ITM.stim[0];
-
-    let mut flash = dp.FLASH.constrain();
-    let mut rcc = dp.RCC.constrain();
-    let gpioe = dp.GPIOE.split(&mut rcc.ahb);
-
-    // clock configuration using the default settings (all clocks run at 8 MHz)
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
-    let mut leds = Leds::new(gpioe);
-    let mut delay = Delay::new(cp.SYST, clocks);
+    let (mut itm, mut leds, mut delay) = oscore_demo::init();
+    let stim = &mut itm.stim[0];
 
     siprintln!(stim, "Initialized and ready.");
-
     oscore_demo::spin_leds(&mut delay, &mut leds);
 
     #[allow(clippy::empty_loop)]
