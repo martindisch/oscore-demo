7415b04be81bf04857cbbb627101601533f4884b
Martin Disch <martindisch@gmail.com>
2019-09-25 12:45:44 +0200

    Add LED example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
 #![no_std]
 
-pub fn panic() {
-    panic!("Oh, snap!");
+use f3::{
+    hal::{delay::Delay, prelude::*},
+    led::Leds,
+};
+
+/// Lights the LEDs in the circle one by one.
+pub fn spin_leds(delay: &mut Delay, leds: &mut Leds) {
+    let n = leds.len();
+    loop {
+        for curr in 0..n {
+            let next = (curr + 1) % n;
+            leds[curr].off();
+            leds[next].on();
+
+            delay.delay_ms(100_u8);
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,34 @@
 
 extern crate panic_itm;
 
-use cortex_m::{iprintln, Peripherals};
+use cortex_m::iprintln;
 use cortex_m_rt::entry;
+use f3::{
+    hal::{delay::Delay, prelude::*, stm32f30x},
+    led::Leds,
+};
 
 #[entry]
 fn main() -> ! {
-    let mut p = Peripherals::take().unwrap();
-    let stim = &mut p.ITM.stim[0];
+    let mut cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f30x::Peripherals::take().unwrap();
+
+    let stim = &mut cp.ITM.stim[0];
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let gpioe = dp.GPIOE.split(&mut rcc.ahb);
+
+    // clock configuration using the default settings (all clocks run at 8 MHz)
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut leds = Leds::new(gpioe);
+    let mut delay = Delay::new(cp.SYST, clocks);
+
     iprintln!(stim, "Initialized and ready.");
 
-    oscore_demo::panic();
+    oscore_demo::spin_leds(&mut delay, &mut leds);
 
+    #[allow(clippy::empty_loop)]
     loop {}
 }
