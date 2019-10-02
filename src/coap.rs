dff69af13bfdc970605d40d31917802a99f13638
Martin Disch <martindisch@gmail.com>
2019-10-02 17:32:57 +0200

    Implement /hello and /echo demo endpoints

--- src/coap.rs
+++ src/coap.rs
@@ -31,6 +31,21 @@ pub fn handle(req: &Packet) -> Packet {
                     req,
                     br#"</.well-known/core>;rt="core";ct=40"#.to_vec(),
                 );
+            } else if first == b"hello" {
+                // Response to /hello
+                return generate_response(
+                    req,
+                    b"Hello, world!".to_vec(),
+                    ContentFormat::TextPlain,
+                );
+            } else if first == b"echo" {
+                // Response to /echo
+                let payload = req.payload.clone();
+                return generate_response(
+                    req,
+                    payload,
+                    ContentFormat::ApplicationOctetStream,
+                );
             }
         }
     }
@@ -41,6 +56,7 @@ pub fn handle(req: &Packet) -> Packet {
     res.header.code = MessageClass::Response(ResponseType::NotFound);
     res.header.message_id = req.header.message_id;
     res.set_token(req.get_token().clone());
+    res.set_content_format(ContentFormat::TextPlain);
     res.payload = b"Not found".to_vec();
 
     res
@@ -54,6 +70,23 @@ fn generate_link_format(req: &Packet, payload: Vec<u8>) -> Packet {
     res.header.message_id = req.header.message_id;
     res.set_token(req.get_token().clone());
     res.set_content_format(ContentFormat::ApplicationLinkFormat);
+    res.payload = payload;
+
+    res
+}
+
+/// Returns a "normal" response with a payload and content-format.
+fn generate_response(
+    req: &Packet,
+    payload: Vec<u8>,
+    cf: ContentFormat,
+) -> Packet {
+    let mut res = Packet::new();
+    res.header.set_type(MessageType::Acknowledgement);
+    res.header.code = MessageClass::Response(ResponseType::Content);
+    res.header.message_id = req.header.message_id;
+    res.set_token(req.get_token().clone());
+    res.set_content_format(cf);
     res.payload = payload;
 
     res
