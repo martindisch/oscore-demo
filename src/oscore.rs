85f7dc40606c026d96c2d2daeeaf42992f93bc16
Martin Disch <martindisch@gmail.com>
2019-10-07 14:04:39 +0200

    Use OSCORE protection

--- src/oscore.rs
+++ src/oscore.rs
@@ -41,9 +41,35 @@ impl OscoreHandler {
         tx: &mut Tx<USART1>,
         req_bytes: &[u8],
     ) -> Option<Vec<u8>> {
-        let req = Packet::from_bytes(req_bytes).expect("Unable to parse CoAP");
+        let mut req =
+            Packet::from_bytes(req_bytes).expect("Unable to parse CoAP");
+        let mut is_oscore = false;
+
+        // Check if the request is OSCORE and we are ready to deal with it
+        if req.get_option(CoapOption::Oscore).is_some()
+            && self.oscore.is_some()
+        {
+            uprintln!(tx, "Unprotecting OSCORE request");
+            is_oscore = true;
+            // Temporarily take the oscore context
+            let mut oscore = self.oscore.take().unwrap();
+            // Unprotect the request and replace the original with it
+            req = Packet::from_bytes(
+                &oscore
+                    .unprotect_request(req_bytes)
+                    .expect("Failed unprotecting request"),
+            )
+            .expect("Unable to parse unprotected request");
+            // Put the context back in
+            self.oscore = Some(oscore);
+        }
+
         // Use CoAP handler to deal with it
-        let res = self.coap.handle(tx, &mut self.edhoc, req)?;
+        let mut res = self
+            .coap
+            .handle(tx, &mut self.edhoc, req)?
+            .to_bytes()
+            .expect("Error building CoAP bytes");
 
         // Check if EDHOC has advanced
         if let Some((master_secret, master_salt)) = self.edhoc.take_params() {
@@ -59,7 +85,20 @@ impl OscoreHandler {
             );
         }
 
+        // If the exchange is protected with OSCORE, protect the response
+        if is_oscore {
+            uprintln!(tx, "Protecting OSCORE response");
+            // Temporarily take the oscore context
+            let mut oscore = self.oscore.take().unwrap();
+            // Protect the response and replace the original with it
+            res = oscore
+                .protect_response(&res, req_bytes, true)
+                .expect("Failed protecting response");
+            // Put the context back in
+            self.oscore = Some(oscore);
+        }
+
         // Return the bytes of the CoAP response packet
-        Some(res.to_bytes().expect("Error building CoAP bytes"))
+        Some(res)
     }
 }
